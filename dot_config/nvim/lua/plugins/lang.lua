85b4d2d331de41e5c9b26292aba20878fb28ce7a
Christoph Schmatzler <christoph@medium.place>
2024-12-14 19:41:25 +0100

    Update

--- dot_config/nvim/lua/plugins/lang.lua
+++ dot_config/nvim/lua/plugins/lang.lua
@@ -1,64 +1,64 @@
 return {
-  -- Swift
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "swift",
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        sourcekit = {
-          capabilities = {
-            workspace = {
-              didChangeWatchedFiles = {
-                dynamicRegistration = true,
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  -- Elixir
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "elixir",
-        "heex",
-        "eex",
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        lexical = {
-          filetypes = { "elixir", "eelixir", "heex" },
-          cmd = { "lexical" },
-          root_dir = function(fname)
-            return require("lspconfig").util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
-          end,
-        },
-      },
-    },
-  },
-  {
-    "nvim-neotest/neotest",
-    optional = true,
-    dependencies = {
-      "jfpedroza/neotest-elixir",
-    },
-    opts = {
-      adapters = {
-        ["neotest-elixir"] = {},
-      },
-    },
-  },
+	-- Swift
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"swift",
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				sourcekit = {
+					capabilities = {
+						workspace = {
+							didChangeWatchedFiles = {
+								dynamicRegistration = true,
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+	-- Elixir
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"elixir",
+				"heex",
+				"eex",
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				lexical = {
+					filetypes = { "elixir", "eelixir", "heex" },
+					cmd = { "lexical" },
+					root_dir = function(fname)
+						return require("lspconfig").util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
+					end,
+				},
+			},
+		},
+	},
+	{
+		"nvim-neotest/neotest",
+		optional = true,
+		dependencies = {
+			"jfpedroza/neotest-elixir",
+		},
+		opts = {
+			adapters = {
+				["neotest-elixir"] = {},
+			},
+		},
+	},
 }
