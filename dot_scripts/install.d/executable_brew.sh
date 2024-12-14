f626dd040b3478d631ea9ee8c70af02902b6a04d
Christoph Schmatzler <christoph@medium.place>
2024-12-14 20:54:37 +0100

    Install personal packages

--- dot_scripts/install.d/executable_brew.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Installing and upgrading brew packages...
-
-cat $HOME/.config/brew/Brewfile.common | brew bundle cleanup --force --file=-
-cat $HOME/.config/brew/Brewfile.common | brew bundle --file=-
-brew upgrade --fetch-HEAD
