390e7149bf6c4b20816c6d26e502a0f9d31f9f43
Christoph Schmatzler <christoph@medium.place>
2025-01-02 22:39:08 +0100

    up
    Signed-off-by: Christoph Schmatzler <christoph@medium.place>

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -1,42 +1,13 @@
 return {
-  {
-    "yetone/avante.nvim",
-    build = ":AvanteBuild",
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
-      {
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        ft = { "markdown", "Avante" },
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-      },
-    },
-    event = "VeryLazy",
-    opts = {
-      hints = {
-        enabled = false,
-      },
-      windows = {
-        width = 36.5,
-      },
-    },
-  },
+	{
+		"supermaven-inc/supermaven-nvim",
+		event = "VeryLazy",
+		opts = {
+			keymaps = {
+				accept_suggestion = "<Tab>",
+				clear_suggestion = "<C-]>",
+				accept_word = "<C-j>",
+			},
+		},
+	},
 }
