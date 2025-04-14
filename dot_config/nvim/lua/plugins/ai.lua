6354a45093a41cccfa531617a1472a8b81bc284f
Christoph Schmatzler <christoph@tuist.dev>
2025-04-14 09:27:27 +0200

    hmm

---
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"supermaven-inc/supermaven-nvim",
+		event = "VeryLazy",
+		opts = {
+			keymaps = {
+				accept_suggestion = "<Tab>",
+				clear_suggestion = "<C-]>",
+				accept_word = "<C-j>",
+			},
+		},
+	},
+}
