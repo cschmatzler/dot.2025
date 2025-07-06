28aab076c2b05b2b49c1d87fb48825a44465dd93
Christoph Schmatzler <christoph@schmatzler.com>
2025-07-06 10:53:32 +0200

    up
    Signed-off-by: Christoph Schmatzler <christoph@schmatzler.com>

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "catppuccin-latte",
+			colorscheme = "catppuccin-macchiato",
 		},
 	},
 	{ "akinsho/bufferline.nvim", enabled = false },
